81038ecb615f21c63708db7930b2c343c749699e
Troels Liebe Bentsen <tlb@nversion.dk>
2015-03-11 18:18:39 +0100

    Added initial code

---
+++ src/dk/nversion/JWT.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.nversion;
+
+import org.json.JSONObject;
+
+/**
+ *
+ * @author tlb
+ */
+public class JWT {
+    public static JSONObject decode(String jwtstring) {
+        return null;
+    }
+    
+    public static String encode(JSONObject header, JSONObject body, String key) {
+        return null;
+    }
+}
